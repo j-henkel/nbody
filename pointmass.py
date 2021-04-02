55aa08233d2404d1a8766d0f44881363c7b818c4
j-henkel <jan.henkel.1@web.de>
2021-04-02 23:17:23 +0200

    added dist method

--- pointmass.py
+++ pointmass.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 
-class Body:
+class PointMass:
+    """point mass"""
+
     def __init__(self, mass: float, position: np.ndarray, velocity: np.ndarray):
         self.mass = mass
         self.position = position
         self.velocity = velocity
         return
+
+    def dist(self, obj):
+        """Calculates the distance to another object"""
+
+        assert np.shape(self.position) == np.shape(obj.position), "positions must be of same dimension/shape"
+        return np.sqrt(np.sum((self.position - obj.position) ** 2))
