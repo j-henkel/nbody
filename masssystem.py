59bf33fb4a43b17b081fc955e227439d564aa12b
j-henkel <jan.henkel.1@web.de>
2021-04-02 23:59:27 +0200

    created masssystem.py

---
+++ masssystem.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class MassSystem:
+    """a gravitational system of several objects"""
+
+    def __init__(self):
+        pass
+
+    def step(self):
+        """calculate the next state of the gravitational system"""
+
+        pass
+
+    def centre_of_mass(self):
+        """calculate the centre of mass"""
+
+        pass
+
+    def fix(self):
+        """define the velocity of the centre of mass as zero"""
+
+        pass
