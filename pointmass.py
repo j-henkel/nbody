2686ccb3dc8d9ee7dfa356846ec93760e3b59fdb
j-henkel <jan.henkel.1@web.de>
2021-04-02 23:39:23 +0200

    added docstrings

--- pointmass.py
+++ pointmass.py
@@ -2,16 +2,54 @@ import numpy as np
 
 
 class PointMass:
-    """point mass"""
+    """point mass
+
+    Attributes
+    ----------
+    mass: float
+        the mass of the object in kg
+    position: np.ndarray
+        the position of the object in m
+    velocity: np.ndarray
+        the velocity of the object in m/s
+
+    Methods
+    -------
+    dist(obj)
+        calculates the distance to another object
+    """
 
     def __init__(self, mass: float, position: np.ndarray, velocity: np.ndarray):
+        """
+        Parameters
+        ----------
+        mass: float
+            the mass of the object in kg
+        position: np.ndarray
+            the position of the object in m
+        velocity: np.ndarray
+            the velocity of the object in m/s
+        """
+
+        assert np.shape(self.position) == np.shape(self.velocity), "position and velocity must be of same shape"
         self.mass = mass
         self.position = position
         self.velocity = velocity
         return
 
     def dist(self, obj):
-        """Calculates the distance to another object"""
+        """Calculates the distance to another object
+
+        Parameters
+        ----------
+        obj: PointMass
+            the object the distance to is calculated
+
+        Raises
+        ------
+        AssertionError
+            if the shapes of self.position and obj.position don't match
+        """
 
         assert np.shape(self.position) == np.shape(obj.position), "positions must be of same dimension/shape"
         return np.sqrt(np.sum((self.position - obj.position) ** 2))
