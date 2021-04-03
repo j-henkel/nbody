d7bbc6dcd705939d26705513255ca0de79bb66a0
j-henkel <jan.henkel.1@web.de>
2021-04-03 21:55:47 +0200

    added TODOs

--- masssystem.py
+++ masssystem.py
@@ -16,20 +16,23 @@ class MassSystem:
 
     def step(self, inplace=True):
         """calculate the next state of the gravitational system"""
+        # TODO write step algorithm
 
         pass
 
     def centre_of_mass(self):
         """calculate the centre of mass"""
+        # TODO centre_of_mass algorithm
 
         pass
 
     def fix(self):
         """define the velocity of the centre of mass as zero"""
-
+        # TODO fix() algorithm
         pass
 
     def get_object(self, name: str):
         """return a PointMass object of the body named name"""
+        # TODO get_object method
 
         pass
