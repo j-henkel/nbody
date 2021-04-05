7af78f643cf71f05a1ed48d3bf050f0bba596110
j-henkel <jan.henkel.1@web.de>
2021-04-05 20:46:32 +0200

    added docstrings to fix() function

--- algorithms.py
+++ algorithms.py
@@ -3,8 +3,22 @@ import numpy as np
 
 def fix(all_velocities: np.ndarray,
         all_masses: np.ndarray) -> np.ndarray:
-    """define the velocity of the centre of mass as zero. Returns the
+    """Define the velocity of the centre of mass as zero. Returns the
     adjusted velocity of all objects within the masssystem.
+
+    Parameters
+    ----------
+    all_velocities: np.ndarray
+        An array that combines the velocity vectors of several bodys,
+        typically a MassSystem.all_velocitys array
+    all_masses: np.ndarray
+        An array that combines the corresponding masses, typically
+        a MassSystem.all_masses array
+
+    Returns
+    -------
+    all_v_new: np.ndarray
+        The adjusted all_velocities array
     """
 
     all_pulses = all_velocities * all_masses[:, None]
@@ -14,3 +28,9 @@ def fix(all_velocities: np.ndarray,
 
     all_v_new = all_velocities - velocity_centre_of_mass
     return all_v_new
+
+
+def centre_of_mass(all_positions: np.ndarray,
+                   all_masses: np.ndarray) -> np.ndarray:
+    """calculate the centre of mass"""
+    pass
